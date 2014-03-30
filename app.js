6c55238dddef7e969bd525838be4123394c2ddcc
Chinmay Patwardhan <chinnychin19@gmail.com>
2014-03-30 04:28:34 -0400

    stupid testing

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 var googleapis = require('googleapis');
-var config = require('./config.js');
+// var config = require('./config.js');
 var app = require('express')();
 
-var CLIENT_ID = config.CLIENT_ID;
-  CLIENT_SECRET = config.CLIENT_SECRET,
-  REDIRECT_URL = config.REDIRECT_URL,
-  SCOPE = config.SCOPE,
-  REFRESH_TOKEN = config.REFRESH_TOKEN,
-  BLANK_FILE_ID = config.BLANK_FILE_ID;
+// var CLIENT_ID = config.CLIENT_ID;
+//   CLIENT_SECRET = config.CLIENT_SECRET,
+//   REDIRECT_URL = config.REDIRECT_URL,
+//   SCOPE = config.SCOPE,
+//   REFRESH_TOKEN = config.REFRESH_TOKEN,
+//   BLANK_FILE_ID = config.BLANK_FILE_ID;
 
-var auth = new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+// var auth = new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
 app.get('/', function (req, res) {
-    createDoc(res);
+    res.send('hello world');
+    //createDoc(res);
   });
 
 app.listen(8080);
