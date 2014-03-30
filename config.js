07393241cf2f199785f2dee6007b24be0b5acf12
Chinmay Patwardhan <chinnychin19@gmail.com>
2014-03-30 04:18:03 -0400

    stopped ignoring config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.CLIENT_ID =     '410683408780-hsl1elp3gp4t9pbrcqfq5p9mo7rujn6m.apps.googleusercontent.com';
+exports.CLIENT_SECRET = 'Xs4vQcTcADDvlX7SQF3FNb7D';
+exports.REDIRECT_URL =  'http://boiling-garden-7787.herokuapp.com/';
+exports.SCOPE =         'https://www.googleapis.com/auth/drive';
+exports.REFRESH_TOKEN = "1/nfxnGAS_EbgJZ5-k77VzWHJFE0cW9R7X-TexKYcs4ow";
+exports.BLANK_FILE_ID = "16ZnTqwVWkAWkd_Ot9Q4gDEKuAU-o4VgjbvqVyYZRVlE";
